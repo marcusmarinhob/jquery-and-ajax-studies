faae77d4c0f3d9cca1f5af697a41d7326237e4de
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-06-27 15:56:19 -0300

    Mostrando logradouro, bairro, localidade e uf na página

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ function consultaCep(){
         type: "GET",
         success: function(response){
             console.log(response);
+            document.getElementById("logradouro").innerHTML = response.logradouro;
+            document.getElementById("bairro").innerHTML = response.bairro;
+            document.getElementById("localidade").innerHTML = response.localidade;
+            document.getElementById("uf").innerHTML = response.uf;
         }
     })
 }
