c5bf6fc2d2881a579e5a264bb7b7beab4d9861be
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-06-27 16:50:19 -0300

    Estilizando a página com o Bootstrap

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ function consultaCep(){
             $("#bairro").html(response.bairro);
             $("#localidade").html(response.localidade);
             $("#uf").html(response.uf);
+            $("#cep-title").html("CEP: " + response.cep);
         }
     })
 }
